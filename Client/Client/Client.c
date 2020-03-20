97b4b37b24f650cf603021302a138ce3ad8bc1de
zhoumi <zm@willshare.cn>
2020-03-20 18:22:54 +0800

    update

--- Client/Client/Client.c
+++ Client/Client/Client.c
@@ -1,70 +1,202 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <winsock2.h>
+//
+//#define BUF_SIZE 1024
+//void ErrorHandling(char* message);
+//
+//int main(int argc, char* argv[]) {
+//	WSADATA wsaData;
+//	SOCKET hSocket;
+//	char message[BUF_SIZE];
+//	int strLen;
+//	SOCKADDR_IN servAdr;
+//
+//	if (argc!=3)
+//	{
+//		printf("Usage : %s<IP><Port>\n", argv[0]);
+//		exit(1);
+//	}
+//
+//	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
+//	{
+//		ErrorHandling("WSAStartup() error!");
+//	}
+//
+//	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+//	memset(&servAdr, 0, sizeof(servAdr));
+//	servAdr.sin_family = AF_INET;
+//	servAdr.sin_addr.S_un.S_addr = inet_addr(argv[1]);
+//	servAdr.sin_port = htons(atoi(argv[2]));
+//
+//	if (connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr))==-1)
+//	{
+//		ErrorHandling("connect() error!");
+//	}
+//	else
+//	{
+//		puts("Connected....");
+//	}
+//
+//	while (1) {
+//		fputs("Input message(Q to quit):", stdout);
+//		fgets(message, BUF_SIZE, stdin);
+//
+//		if (!strcmp(message,"q\n")||!(strcmp(message,"Q\n")))
+//		{
+//			break;
+//		}
+//
+//		send(hSocket, message, strlen(message), 0);
+//		strLen = recv(hSocket, message, BUF_SIZE - 1, 0);
+//		message[strLen] = 0;
+//		printf("Message from server: %s", message);
+//		
+//
+//	}
+//
+//	
+//	closesocket(hSocket);
+//	WSACleanup();
+//	return 0;
+//}
+//
+//void ErrorHandling(char* message) {
+//	fputs(message, stderr);
+//	fputc('\n', stderr);
+//	exit(1);
+//}
 
-#define BUF_SIZE 1024
-void ErrorHandling(char* message);
+///* 第14章 */
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//
+//#define BUF_SIZE 30
+//
+//int main(int argc,char* argv[])
+//{
+//	WSADATA wsaData;
+//	SOCKET hRecvSock;
+//	SOCKADDR_IN adr;
+//	struct ip_mreq joinAdr;
+//	char buf[BUF_SIZE];
+//	int strLen;
+//
+//	if (argc!=3)
+//	{
+//		printf("argc not right!");
+//		exit(1);
+//	}
+//
+//	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
+//	{
+//		printf("WSAStartup error!");
+//	}
+//
+//	hRecvSock = socket(PF_INET, SOCK_DGRAM, 0);
+//	memset(&adr, 0, sizeof(adr));
+//	adr.sin_family = AF_INET;
+//	adr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+//	adr.sin_port = htons(atoi(argv[2]));
+//	if (bind(hRecvSock,(SOCKADDR*)&adr,sizeof(adr))==SOCKET_ERROR)
+//	{
+//		printf("Bind() error!");
+//	}
+//
+//	joinAdr.imr_multiaddr.S_un.S_addr = inet_addr(argv[1]);
+//	joinAdr.imr_interface.S_un.S_addr = htonl(INADDR_ANY);
+//	if (setsockopt(hRecvSock,IPPROTO_IP,IP_ADD_MEMBERSHIP,(void*)&joinAdr,sizeof(joinAdr))==SOCKET_ERROR)
+//	{
+//		printf("setsock() error!");
+//	}
+//
+//	while (1)
+//	{
+//		strLen = recvfrom(hRecvSock, buf, BUF_SIZE - 1, 0, NULL, 0);
+//		if (strLen<0)
+//		{
+//			break;
+//		}
+//
+//		buf[strLen] = 0;
+//		fputs(buf, stdout);
+//
+//	}
+//	closesocket(hRecvSock);
+//	WSACleanup();
+//	return 0;
+//}
 
-int main(int argc, char* argv[]) {
-	WSADATA wsaData;
-	SOCKET hSocket;
-	char message[BUF_SIZE];
-	int strLen;
-	SOCKADDR_IN servAdr;
+///* 第15章 */
+//
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <winsock2.h>
+//#include <WS2tcpip.h>
+//
+//#define BUF_SIZE 1024
+//
+//int main(int argc, char* argv[]) {
+//	WSADATA wsaData;
+//	SOCKET sock;
+//	char message[BUF_SIZE];
+//	SOCKADDR_IN serv_adr;
+//	FILE* readfp;
+//	FILE* writefp;
+//
+//	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		printf("WSAStartup() error!");
+//
+//	if (argc != 3)
+//		printf("start input error!");
+//
+//	sock = socket(PF_INET, SOCK_STREAM, 0);
+//	if (sock == -1)
+//		printf("sock init error!");
+//	memset(&serv_adr, 0, sizeof(serv_adr));
+//	serv_adr.sin_family = AF_INET;
+//	inet_pton(AF_INET, "127.0.0.1", (void*)&serv_adr.sin_addr.S_un.S_addr);
+//	serv_adr.sin_addr.S_un.S_addr = inet_addr(argv[1]);
+//	
+//	serv_adr.sin_port = htons(atoi(argv[2]));
+//
+//	if (connect(sock, (SOCKADDR*) &serv_adr, sizeof(serv_adr)) == SOCKET_ERROR)
+//		printf("connect() error!");
+//	else
+//		printf("Connected.......");
+//
+//	
+//	readfp = fopen(sock, "r");
+//	writefp = fopen(sock, "w");
+//
+//	while (1)
+//	{
+//		fputs("input message(Q to quit):", stdout);
+//		fgets(message, BUF_SIZE, stdin);
+//		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+//			break;
+//
+//		fputs(message, writefp);
+//		fflush(writefp);
+//		fgets(message, BUF_SIZE, readfp);
+//		printf("Message from server:%s", message);
+//	}
+//	if (writefp!=NULL)
+//	{
+//		fclose(writefp);
+//	}
+//	if (readfp != NULL)
+//	{
+//		fclose(readfp);
+//	}
+//	
+//	return 0;
+//
+//}
 
-	if (argc!=3)
-	{
-		printf("Usage : %s<IP><Port>\n", argv[0]);
-		exit(1);
-	}
-
-	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
-	{
-		ErrorHandling("WSAStartup() error!");
-	}
-
-	hSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.S_un.S_addr = inet_addr(argv[1]);
-	servAdr.sin_port = htons(atoi(argv[2]));
-
-	if (connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr))==-1)
-	{
-		ErrorHandling("connect() error!");
-	}
-	else
-	{
-		puts("Connected....");
-	}
-
-	while (1) {
-		fpus("Input message(Q to quit):", stdout);
-		fgets(message, BUF_SIZE, stdin);
-
-		if (!strcmp(message,"q\n")||!(strcmp(message,"Q\n")))
-		{
-			break;
-		}
-
-		send(hSocket, message, strlen(message), 0);
-		strLen = recv(hSocket, message, BUF_SIZE - 1, 0);
-		message[strLen] = 0;
-		printf("Message from server: %s", message);
-		
-
-	}
-
-	
-	closesocket(hSocket);
-	WSACleanup();
-	return 0;
-}
-
-void ErrorHandling(char* message) {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
--- Server/Server/Server.c
+++ Server/Server/Server.c
@@ -133,124 +133,277 @@
 //	exit(1);
 //}
 
-/* 第12章 */
+///* 第12章 */
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <winsock2.h>
+//
+//#define BUF_SIZE 1024
+//
+//void ErrorHandling(char* message);
+//
+//int main(int argc, char* argv[]) {
+//	WSADATA wsaData;
+//	SOCKET hServSock, hClntSock;
+//	SOCKADDR_IN servAdr, clntAdr;
+//	TIMEVAL timeout;
+//	fd_set reads, cpyReads;
+//
+//	int adrSz;
+//	int strLen, fdNum, i;
+//	char buf[BUF_SIZE];
+//
+//	
+//	if (argc!=2)
+//	{
+//		printf("Usage: %s <port>\n", argv[0]);
+//		exit(1);
+//	}
+//
+//	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
+//	{
+//		ErrorHandling("WSAStartup() error!");
+//	}
+//	
+//	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+//	memset(&servAdr, 0, sizeof(servAdr));
+//	servAdr.sin_family = AF_INET;
+//	servAdr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);	
+//	servAdr.sin_port = htons(atoi(argv[1]));
+//	
+//	
+//	if (bind(hServSock,(SOCKADDR*)&servAdr,sizeof(servAdr))== SOCKET_ERROR)
+//	{
+//		ErrorHandling("bind() error");
+//	}
+//
+//	if (listen(hServSock,5)==SOCKET_ERROR)
+//	{
+//		ErrorHandling("listen() error");
+//	}
+//
+//	FD_ZERO(&reads);
+//	FD_SET(hServSock, &reads);
+//
+//	while (1)
+//	{
+//		cpyReads = reads;
+//
+//		timeout.tv_sec = 5;
+//		timeout.tv_usec = 5000;
+//
+//		if ((fdNum=select(0,&cpyReads,0,0,&timeout))== SOCKET_ERROR)
+//		{
+//			
+//			break;
+//		}
+//
+//		if (fdNum==0)
+//		{
+//			
+//			continue;
+//		}
+//		
+//		for (i = 0; i < reads.fd_count; i++) {
+//			if (FD_ISSET(reads.fd_array[i],&cpyReads))
+//			{
+//				if (reads.fd_array[i]==hServSock)
+//				{
+//					adrSz = sizeof(clntAdr);
+//					hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &adrSz);
+//					FD_SET(hClntSock, &reads);
+//					printf("connected clinet: %d \n,hClntSock");
+//				}
+//				else
+//				{
+//					strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
+//					if (strLen == 0)
+//					{
+//						FD_CLR(reads.fd_array[i], &reads);
+//						closesocket(cpyReads.fd_array[i]);
+//						printf("closed client: %d \n", cpyReads.fd_array[i]);
+//					}
+//					else
+//					{
+//						send(reads.fd_array[i], buf, strLen, 0);
+//					}
+//				}
+//				
+//			}
+//		}
+//
+//
+//	}
+//	closesocket(hServSock);
+//	WSACleanup();	
+//	return 0;
+//
+//}
+//
+//void ErrorHandling(char* message) {
+//	fputs(message, stderr);
+//	fputc("\n", stderr);
+//	exit(1);
+//}
+
+///* 第14章 */
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//
+//#define TTL 64
+//#define BUF_SIZE 30
+//void ErrorHandling(char* message);
+//
+//int main(int argc, char* argv[]) {
+//	WSADATA wsaData;
+//	SOCKET hSendSock;
+//	SOCKADDR_IN mulAdr;
+//	int timeLive=TTL;
+//	FILE* fp;
+//	char buf[BUF_SIZE];
+//
+//	if (argc!=3)
+//	{
+//		printf("argc not right!");
+//		exit(1);
+//	}
+//
+//	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
+//	{
+//		printf("WSAStartup error!");
+//		exit(1);
+//	}
+//
+//	hSendSock = socket(PF_INET, SOCK_DGRAM, 0);
+//	memset(&mulAdr, 0, sizeof(mulAdr));
+//	mulAdr.sin_family = AF_INET;
+//	mulAdr.sin_addr.S_un.S_addr = inet_addr(argv[1]);
+//	mulAdr.sin_port = htons(atoi(argv[2]));
+//
+//	setsockopt(hSendSock, IPPROTO_IP, IP_MULTICAST_TTL, (void*)&timeLive, sizeof(timeLive));
+//	
+//	if ((fp=fopen("new.txt","r"))==NULL)
+//	{
+//		printf("fopen error");
+//	}
+//
+//	while (!feof(fp))
+//	{
+//		fgets(buf, BUF_SIZE, fp);
+//		sendto(hSendSock, buf, strlen(buf), 0, (SOCKADDR*)&mulAdr, sizeof(mulAdr));
+//		Sleep(2000);
+//	}
+//	closesocket(hSendSock);
+//	WSACleanup();
+//	return 0;
+//
+//}
+
+/* 第15章 */
+
+/*  */
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <winsock2.h>
+//#include <string.h>
+//
+//#define BUF_SIZE 1024
+//
+//int main(int argc, char* argv[]) {
+//	WSADATA wsaData;
+//	char message[BUF_SIZE];
+//	FILE* readfp;
+//	FILE* writefp;
+//	SOCKET sevr_sock;
+//    SOCKET clnt_sock;
+//	SOCKADDR_IN serv_adr;
+//	SOCKADDR_IN clnt_adr;
+//	int clnt_adr_size;
+//
+//	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		printf("WSAStartup() error!");
+//
+//	if (argc != 2)
+//		printf("start para error!");
+//
+//	sevr_sock = socket(PF_INET, SOCK_STREAM, 0);
+//	if (sevr_sock == -1)
+//		printf("serv_sock init error!");
+//		
+//	memset(&serv_adr, 0, sizeof(serv_adr));
+//	serv_adr.sin_family = AF_INET;
+//	serv_adr.sin_addr.S_un.S_addr = htonl(INADDR_ANY); 
+//	serv_adr.sin_port = htons(atoi(argv[1]));
+//	                                     
+//	if (bind(sevr_sock, (SOCKADDR*) & serv_adr, sizeof(serv_adr)) == -1)
+//		printf("bind() error!");
+//
+//	if (listen(sevr_sock, 5) == -1)
+//		printf("listen error!");
+//	clnt_adr_size = sizeof(clnt_adr);
+//	for (size_t i = 0; i < 5; i++)
+//	{
+//		clnt_sock = accept(sevr_sock, (SOCKADDR*) &clnt_adr, &clnt_adr_size);
+//		if (clnt_sock == -1)
+//			printf("accept() error!");
+//		else 
+//			printf("Connecte client %d \n", i + 1);		
+//
+//		readfp = fopen(clnt_sock, "r");
+//		writefp = fopen(clnt_sock, "w");
+//		while (!feof(readfp))
+//		{
+//			fgets(message, BUF_SIZE, readfp);
+//			fputs(message, writefp);
+//			fflush(writefp);
+//		}
+//		fclose(readfp);
+//		fclose(writefp);
+//	}
+//	close(sevr_sock);
+//	return 0;
+//}
+
+/* 第19章 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
+#include <windows.h>
+#include <process.h>
+unsigned WINAPI ThreadFunc(void* arg);
 
-#define BUF_SIZE 1024
+int main(int argc, char* argv[]) {
+	HANDLE hThread;
+	unsigned threadID;
+	int param = 5;
 
-void ErrorHandling(char* message);
-
-int main(int argc, char* argv) {
-	WSADATA wsaData;
-	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAdr, clntAdr;
-	TIMEVAL timeout;
-	fd_set reads, cpyReads;
-
-	int adrSz;
-	int strLen, fdNum, i;
-	char buf[BUF_SIZE];
-
-	printf("开始");
-
-	if (argc!=2)
+	hThread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, (void*)&param, 0, &threadID);
+	if (hThread==0)
 	{
-		printf("Usage: %s <port>\n", argv[0]);
-		exit(1);
+		puts("_beginThreadex() error");
+		return -1;
 	}
 
-	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
-	{
-		ErrorHandling("WSAStartup() error!");
-	}
-	
-	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	
-	servAdr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-	
-	servAdr.sin_port = htons(atoi(argv[1]));
-	printf("到这");
-	
-	if (bind(hServSock,(SOCKADDR*)&servAdr,sizeof(servAdr))== SOCKET_ERROR)
-	{
-		ErrorHandling("bind() error");
-	}
-
-	if (listen(hServSock,5)==SOCKET_ERROR)
-	{
-		ErrorHandling("listen() error");
-	}
-
-	FD_ZERO(&reads);
-	FD_SET(hServSock, &reads);
-
-	while (1)
-	{
-		cpyReads = reads;
-		timeout.tv_sec = 5;
-		timeout.tv_usec = 5000;
-
-		if ((fdNum=select(0,&cpyReads,0,0,&timeout))== SOCKET_ERROR)
-		{
-			printf("停止循环");
-			break;
-		}
-
-		if (fdNum==0)
-		{
-			printf("跳过");
-			continue;
-		}
-		
-		for (i = 0; i < reads.fd_count; i++) {
-			if (FD_ISSET(reads.fd_array[i],&cpyReads))
-			{
-				if (reads.fd_array[i]==hServSock)
-				{
-					adrSz = sizeof(clntAdr);
-					hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &adrSz);
-					FD_SET(hClntSock, &reads);
-					printf("connected clinet: %d \n,hClntSock");
-				}
-				else
-				{
-					strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
-					if (strLen == 0)
-					{
-						FD_CLR(reads.fd_array[i], &reads);
-						closesocket(cpyReads.fd_array[i]);
-						printf("closed client: %d \n", cpyReads.fd_array[i]);
-					}
-					else
-					{
-						send(reads.fd_array[i], buf, strLen, 0);
-					}
-
-				}
-				
-			}
-		}
-
-
-	}
-	closesocket(hServSock);
-	WSACleanup();
-	printf("结束");
+	Sleep(3000);
+	puts("end of main");
 	return 0;
+}
 
+unsigned WINAPI ThreadFunc(void *arg) {
+	int i;
+	int cnt = *((int*)arg);
+	for (size_t i = 0; i < cnt; i++)
+	{
+		Sleep(1000); 
+		puts("running thread");
+	}
+	return 0;
 }
 
 
 
-void ErrorHandling(char* message) {
-	fputs(message, stderr);
-	fputc("\n", stderr);
-	exit(1);
-}
